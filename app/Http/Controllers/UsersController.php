64b3268cc7524dea303bd01f493f27bed4222166
汤大胖子 <oncwnuOh5AYQpRpI3IU7nvBU5hXg@git.weixin.qq.com>
2020-01-10 10:28:48 +0800

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['create','show','store']]);
+        $this->middleware('guest',['only'=>'create']);
+
+    }
+
     public function create(){
         return view('users.create');
     }
@@ -34,10 +41,12 @@ class UsersController extends Controller
     }
 
     public function edit(User $user){
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     public function update(User $user,Request $request){
+        $this->authorize('update',$user);
         $request->validate([
             'name' => 'required|max:50',
             'password'=>'nullable|confirmed|min:6'
