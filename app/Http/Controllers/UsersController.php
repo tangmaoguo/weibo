26cff4f9ed56206741240c697c1e0a446b694211
汤大胖子 <oncwnuOh5AYQpRpI3IU7nvBU5hXg@git.weixin.qq.com>
2020-01-15 11:03:49 +0800

    邮件发送

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -78,13 +78,11 @@ class UsersController extends Controller
     protected function sendEmailConfirmationTo($user){
         $view = 'users.confirm_email';
         $data = compact('user');
-        $from = '1096301477@qq.com';
-        $name = 'tangmaoguo';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view,$data,function($message) use ($from,$name,$to,$subject){
-           $message->from($from,$name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
