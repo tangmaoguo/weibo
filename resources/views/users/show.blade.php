a478fde7b6940c396324d0ae79e28239568f6caf
vagrant <vagrant@homestead>
2020-01-19 09:52:41 +0000

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,6 +9,9 @@
                     <section class="user_info">
                         @include('users._user_info',['user'=>$user])
                     </section>
+                    <section class="stats mt-2">
+                        @include('layouts._stats', ['user' => $user])
+                    </section>
                     <section class="status">
                         @if($statuses->count() > 0)
                             <ul class="list-unstyled">
